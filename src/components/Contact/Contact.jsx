62696ce1c02d2e2b57405f0c0435bae379cc3fac
Mohamed Fathi <fathimohamed2710@gmail.com>
2024-07-11 14:15:05 +0000

    'emailPro'

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
       setResult("Sending....");
       const formData = new FormData(event.target);
   
-      formData.append("access_key", "86a69794-34cf-45e4-a500-789375695262");
+      formData.append("access_key", "0615fe39-29e9-486e-bfb4-c7eea2bf33b7");
   
       const response = await fetch("https://api.web3forms.com/submit", {
         method: "POST",
@@ -39,7 +39,7 @@ const Contact = () => {
         <h3>Envoyez-nous un message <img src={msg_icon} /></h3>
         <p>Nous sommes là pour répondre à vos questions, discuter de vos projets ou vous fournir toute l'assistance nécessaire. N'hésitez pas à nous contacter pour toute demande d'information supplémentaire. Nous attendons avec impatience de vous aider à trouver des solutions adaptées à vos besoins.</p>
         <ul>
-            <li><img src={mail_icon} />Contact@gmail.com</li>
+            <li><img src={mail_icon} />contact@sosgroup-maintenance.com</li>
             <li><img src={phone_icon} />+228 99 29 6363 <br/> +228 99 25 2323</li>
             <li><img src={adresse_icon} />Route du countournement, Attiégou</li>
         </ul>
@@ -48,6 +48,8 @@ const Contact = () => {
         <form onSubmit={onSubmit}>
             <label>Votre Nom</label>
             <input type="text" name='name' placeholder='Entrez votre nom' required/>
+            <label>Votre Email</label>
+            <input type="text" name='email' placeholder='Entrez votre Email' required/>
             <label>Numéro de téléphone</label>
             <input type="tel" name='phone' placeholder='Entrez votre Numéro de téléphone' required/>
             <label>Écrivez vos messages ici</label>
