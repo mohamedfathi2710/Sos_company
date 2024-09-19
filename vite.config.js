54645241c22923e6ae05f6c06513c12e3d111243
Mohamed Fathi <fathimohamed2710@gmail.com>
2024-09-19 17:30:21 +0000

    'test3'

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', // Ensure this matches the "distDir" in your vercel.json
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
-});
+  plugins: [react()],
+})
